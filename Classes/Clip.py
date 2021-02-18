bf5d77d8021e6e96a19313fdb46d2292de6ffaaa
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2021-02-18 00:16:17 -0500

    Date parsing added.

--- Classes/Clip.py
+++ Classes/Clip.py
@@ -1,3 +1,5 @@
+import datetime
+
 
 class Clip:
 
@@ -5,3 +7,15 @@ class Clip:
 
         self.parent_dir = parent_dir
         self.filename = filename
+
+        self.dt = self.get_dt()
+
+    def get_dt(self):
+        temp_str = " ".join(self.filename.split(" ")[1:])
+        temp_str = ".".join(temp_str.split(".")[:-2])
+        return datetime.datetime.strptime(temp_str, "%Y.%m.%d - %H.%M.%S.%f")
+
+    def __str__(self):
+        return "Clip - {}".format(
+            self.dt
+        )
